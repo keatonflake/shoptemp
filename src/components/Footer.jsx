a972f3d6dd0d80f429738c285208dc7c48693a86
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2025-01-27 22:24:24 -0700

    configured tailwind and set up structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="absolute bottom-0 bg-slate-500 w-screen">
+      <h1 className="p-4 text-white">Keaton Flake</h1>
+    </footer>
+  );
+};
